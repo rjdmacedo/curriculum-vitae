69a766f5b213ade00f022ba593e14d7fc12f06f2
Rafael Macedo <rafaelmacedo4@gmail.com>
2024-01-06 19:35:02 +0000

    feat: add vite svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import CountBtn from '@/components/CountBtn';
 import ReactSVG from '@/assets/react.svg';
+import ViteSVG from '@/assets/vite.svg';
 import { Badge } from '@/components/ui/badge';
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
         <div className="inline-flex items-center gap-x-4">
           <img src={ReactSVG} alt="React Logo" className="w-32" />
           <span className="text-6xl">+</span>
-          <img src={'/vite.svg'} alt="Vite Logo" className="w-32" />
+          <img src={ViteSVG} alt="Vite Logo" className="w-32" />
         </div>
         <a href="https://ui.shadcn.com" rel="noopener noreferrer nofollow" target="_blank">
           <Badge variant="outline">shadcn/ui</Badge>
