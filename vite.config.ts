b3991064ea9b7657c0bdbe22432f3c728de11587
Rafael Macedo <rafaelmacedo4@gmail.com>
2024-01-06 19:32:46 +0000

    feat: add base to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/curriculum-vitae',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
