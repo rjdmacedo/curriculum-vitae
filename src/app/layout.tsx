a735f20efbfc6be73e145f4bbbe289e65eae3647
Rafael Macedo <Rafael.Macedo@contracted.pmi.com>
2024-06-26 16:02:35 +0100

    feat: add events to posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import {Inter} from "next/font/google";
-import {Analytics} from "@vercel/analytics/react";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 import AppProviders from "@/app/app-providers";
 
 import "./globals.css";
-import {PosthogPageviewTracker} from "@/components/posthog-pageview-tracker";
+import { PosthogPageviewTracker } from "@/components/posthog-pageview-tracker";
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({
