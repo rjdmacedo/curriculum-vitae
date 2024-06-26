2443410c9c11b06b0f31174d0e01abf38e4d128f
Rafael Macedo <Rafael.Macedo@contracted.pmi.com>
2024-06-26 18:25:33 +0100

    chore: remove conditional rendering

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,6 @@
 "use client";
 
 export function Footer() {
-  const buildNumber = process.env.NEXT_PUBLIC_BUILD_NUMBER;
-
-  if (!buildNumber) {
-    return null;
-  }
-
   return (
     <footer className="my-2 text-center text-xs text-muted-foreground">
       <p>build: {process.env.NEXT_PUBLIC_BUILD_NUMBER}</p>
