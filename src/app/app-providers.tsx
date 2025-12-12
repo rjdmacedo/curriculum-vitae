664209bce6608455dec52501b8b051d92caaefee
Rafael Macedo <4865183+rjdmacedo@users.noreply.github.com>
2025-12-12 02:19:34 +0000

    chore: add vercel speed insights

--- src/app/app-providers.tsx
+++ src/app/app-providers.tsx
@@ -4,6 +4,7 @@ import React from "react";
 
 import { Analytics } from "@vercel/analytics/react";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function AppProviders({
   children,
@@ -17,6 +18,7 @@ export default function AppProviders({
       <GoogleAnalytics
         gaId={(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID as string) || ""}
       />
+      <SpeedInsights />
     </>
   );
 }
